8c95c50760742c7bc7358fee35c8f09c5ab5d6e9
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-15 17:49:28 +0100

    Added ReactDom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
 import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
 import { mapAPI } from "./../app.config";
+
 mapboxgl.workerClass = MapboxWorker;
-mapboxgl.accessToken =
-	"pk.eyJ1IjoiYmVhcjk5YTkiLCJhIjoiY2tuajExbHhiMGZiaDJwcDhtYmhiMmV5NiJ9.c9EKTo-Ej1RuD_xuipTTgQ";
+mapboxgl.accessToken = `${mapAPI}`;
+
+const Map = () => {};
+
+ReactDOM.render(<Map />, document.getElementById("app"));
