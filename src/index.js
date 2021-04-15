6ecca3ecb864781860e73868cebe4b6c0ed95c46
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-15 17:54:21 +0100

    Added css and rendered map

--- src/index.js
+++ src/index.js
@@ -7,6 +7,27 @@ import { mapAPI } from "./../app.config";
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = `${mapAPI}`;
 
-const Map = () => {};
+const Map = () => {
+	const mapContainer = useRef();
+	const [lng, setLng] = useState(-70.9);
+	const [lat, setLat] = useState(42.35);
+	const [zoom, setZoom] = useState(9);
+
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [lng, lat],
+			zoom: zoom,
+		});
+		return () => map.remove();
+	}, []);
+
+	return (
+		<div>
+			<div className="map-container" ref={mapContainer} />
+		</div>
+	);
+};
 
 ReactDOM.render(<Map />, document.getElementById("app"));
