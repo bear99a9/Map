94313e726805049d9ae28db220fc2581aa754e1c
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-15 18:02:39 +0100

    Updated lon/ lat and added sidebar with CSS

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React, { useRef, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
 import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
-import { mapAPI } from "./../app.config";
+import { mapAPI } from "./app.config";
 
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = `${mapAPI}`;
 
 const Map = () => {
 	const mapContainer = useRef();
-	const [lng, setLng] = useState(-70.9);
-	const [lat, setLat] = useState(42.35);
+	const [lng, setLng] = useState(-2.2446022);
+	const [lat, setLat] = useState(51.3498525);
 	const [zoom, setZoom] = useState(9);
 
 	useEffect(() => {
@@ -20,11 +20,19 @@ const Map = () => {
 			center: [lng, lat],
 			zoom: zoom,
 		});
+		map.on("move", () => {
+			setLng(map.getCenter().lng.toFixed(4));
+			setLat(map.getCenter().lat.toFixed(4));
+			setZoom(map.getZoom().toFixed(2));
+		});
 		return () => map.remove();
 	}, []);
 
 	return (
 		<div>
+			<div className="sidebar">
+				Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+			</div>
 			<div className="map-container" ref={mapContainer} />
 		</div>
 	);
